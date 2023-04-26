b9f0d5a579510cdd1d3a225fb2e15e8f57ca7c01
divya <dp41532555@gmail.com>
2023-04-26 16:05:36 +0530

    form add bootstrap

---
+++ rohan_project/src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+    return(
+        <div>
+            <div classNameName="card" style={{width: '18rem'}}>
+  <div classNameName="card-body">
+    <h5 classNameName="card-title">Card title</h5>
+    <p classNameName="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" classNameName="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        </div>
+    )
+}
+export default Card
--- rohan_project/src/main.jsx
+++ rohan_project/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// import './index.css'
+import "jquery"
+import "@popperjs/core/dist/umd/popper"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
